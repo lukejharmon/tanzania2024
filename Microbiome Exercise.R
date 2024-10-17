b56cc7e3993e62f4048af8f418617ddb65a435ba
Eliza Tarimo <89925575+elizatarimo@users.noreply.github.com>
2024-10-17 04:02:51 -0400

    Add files via upload
    Microbiome exercise

---
+++ Microbiome Exercise.R
@@ -0,0 +1,11 @@
+#afternoon session 
+#practice your new R skills!
+
+#name and read your data file in R (file is called "Microbiome.csv")
+#pick and name a variable in that dataset
+#use R function to generate statistics for a single variable: generate the mean, standard deviation, standard error, 95% confidence limits of the variable you picked
+#plot your variable using the hist() function
+#pick and name another variable
+#run a t-test to test for a difference in means of the two groups or variables
+#
+
